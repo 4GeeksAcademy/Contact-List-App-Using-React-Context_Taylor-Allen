bc482d1dec30c52249231561cd0819d8e7c8e6e5
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-02-21 02:52:43 +0000

    fixed footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-  <footer
-    className="footer mt-auto py-3 text-center"
-    style={{ color: "#28549f" }}
-  >
+  <footer className="footer mt-auto text-center" style={{ color: "#28549f" }}>
     <p>
       Made with <i className="fa fa-heart text-danger" /> by{" "}
       <a href="https://github.com/taylor-allen">Taylor Allen</a>
--- src/js/layout.js
+++ src/js/layout.js
@@ -32,8 +32,9 @@ const Layout = () => {
 
             <Route path="*" element={<h1>Not found!</h1>} />
           </Routes>
-
-          <Footer />
+          <div className="d-flex justify-content-center">
+            <Footer />
+          </div>
         </ScrollToTop>
       </BrowserRouter>
     </div>
